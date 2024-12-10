cafe18ed5edf8fde4d0a8659598349c228a15323
Nouzen-s <ryuu.seryu@gmail.com>
2024-12-10 14:55:23 +0800

    add model buku

--- server/model.py
+++ server/model.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from app import app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 db = SQLAlchemy(app)
 
+
+class Buku(db.Model):
+    __tablename__ = "buku"
+    id = db.Column(db.Integer, primary_key=True)
+    judul = db.Column(db.String(100),nullable=False)
+    pengarang = db.Column(db.String(100),nullable=False)
+    penerbit = db.Column(db.String(100),nullable=False)
+    genre = db.Column(db.String(100),nullable=False)
+    jenis_buku = db.Column(db.String(100),nullable=False)
